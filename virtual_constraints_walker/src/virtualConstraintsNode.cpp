18e43bdd1f253ec1ba86d66af076adc333cd3337
francesco.ruscelli <francesco.ruscelli@iit.it>
2019-06-05 19:02:30 +0200

    only walking straight, baseline for experiments

--- virtual_constraints_walker/src/virtualConstraintsNode.cpp
+++ virtual_constraints_walker/src/virtualConstraintsNode.cpp
@@ -396,11 +396,11 @@ double virtualConstraintsNode::sense_q1()
     double offset_q1;
     if (_step_counter >= 4+1  && _step_counter < 5+1) /*Left*/
     {
-        offset_q1 = 0.001;
+        offset_q1 = 0.05;
     }
     else if (_step_counter >= 5+1 && _step_counter < 6+1) /*Right*/
     {
-        offset_q1 = 0.02;
+        offset_q1 = 0.05;
     }
     else if (_step_counter >= 6+1 && _step_counter < 7+1) /*Left*/
     {
@@ -424,7 +424,7 @@ double virtualConstraintsNode::sense_q1()
     }    
     else       
     {
-        offset_q1 = - 0.05;
+        offset_q1 = 0.05;
     }
     
         q1 = atan(dist_com(0)/ fabs(stance_ankle_to_com(2))) - offset_q1; // HACK
@@ -1312,7 +1312,7 @@ bool virtualConstraintsNode::compute_step(Step step_type)
                                 sin(theta), cos(theta);
                 if (_step_counter >= 4 && _step_counter < 5)       /*Left*/
                 {
-                    _q1_max = 0.001;
+                    _q1_max = 0.05;
                     q1_max_new = _q1_max; // change step length
                     double theta = _theta_steer; // change heading
                     R_steer << cos(theta), -sin(theta),
@@ -1320,7 +1320,7 @@ bool virtualConstraintsNode::compute_step(Step step_type)
                 }
                 else if (_step_counter >= 5 && _step_counter < 6)   /*Right*/
                 {
-                    _q1_max = 0.02;
+                    _q1_max = 0.05;
                     q1_max_new = _q1_max; // change step length
                     double theta = _theta_steer; // change heading
                     R_steer << cos(theta), -sin(theta),
@@ -1368,7 +1368,7 @@ bool virtualConstraintsNode::compute_step(Step step_type)
                 }
                 else
                 {
-                    _q1_max = - 0.05;
+                    _q1_max = 0.05;
                     q1_max_new = _q1_max;
                     double theta = 0;
                     R_steer << cos(theta), -sin(theta),
