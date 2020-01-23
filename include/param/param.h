ae188a8353febc9f5bef7c7f5540a1850d9af034
francesco.ruscelli <francesco.ruscelli@iit.it>
2020-01-23 17:40:25 +0100

    refactored param file

--- include/param/param.h
+++ include/param/param.h
@@ -9,6 +9,7 @@ class Walker::Param
 {
 public:
 
+    typedef std::shared_ptr<Param> Ptr;
     /**
     * @brief Default (empty) problem
     */
@@ -24,54 +25,41 @@ public:
      */
     Param(ros::NodeHandle nh);
 
-//    double getInitialLowering() const {return _initial_lowering;}
-//    double getStepClearance() const {return _clearance_height;}
-//    double getStepDuration() const {return _step_duration;}
-//    bool getFirstSide() const {return _first_side;}
-//    int getMaxSteps() const {return _max_steps;}
-//    double getZmpOffset() const {return _zmp_offset;}
-//    double getDurationDoubleStance() const {return _duration_double_stance;}
-//    double getStartTime() const {return _start_time;}
-//    double getLeanForward() const {return _lean_forward;}
-//    bool getRealImpactFlag() const {return _real_impacts;}
-//    double getMaxInclination() const {return _max_inclination;}
-//    double getMpcQ() const {return _mpc_Q;}
-//    double getMpcR() const {return _mpc_R;}
-//    bool getDurationPreviewWindow() const {return _duration_preview_window;}
 
-//    void setInitialLowering(double initial_lowering) {_initial_lowering = initial_lowering;}
-//    void setStepClearance(double clearance_height) {_clearance_height = clearance_height;}
-//    void setStepDuration(double step_duration) {_step_duration = step_duration;}
-//    void setFirstSide(Walker::Side first_side) {_first_side = first_side;}
-//    void setMaxSteps(int max_steps) {_max_steps = max_steps;}
-//    void setDurationDoubleStance(double duration_double_stance) {_duration_double_stance = _duration_double_stance;}
-//    void setOffsetZmp(double offset_zmp) {_offset_zmp = offset_zmp;}
-//    void setStartTime(double start_time) {_start_time = start_time;}
-//    void setLeanForward(double lean_forward) {_lean_forward = lean_forward;}
-//    void setThresholdImpactRight(std::vector<double> threshold_impact_right) {_thresholds_impact_right = threshold_impact_right;}
-//    void setThresholdImpactLeft(std::vector<double> threshold_impact_left) {_thresholds_impact_left = threshold_impact_left;}
-//    void setRealImpactFlag(bool real_impacts) {_real_impacts = real_impacts;}
-//    void setMaxInclination(double max_inclination) {_max_inclination = max_inclination;}
-//    void setMpcQ(double mpc_Q) {_mpc_Q = mpc_Q;}
-//    void setMpcR(double mpc_R) {_mpc_R = mpc_R;}
-//    void setDurationPreviewWindow(double duration_preview_window) {_duration_preview_window = duration_preview_window;}
+    double getInitialLowering() const {return _initial_lowering;}
+    bool getFirstSideStep() const {return _first_side_step;}
+    double getLeanForward() const {return _lean_forward;}
+
+    double getStepClearance() const {return _step_clearance;}
+    double getStepDuration() const {return _step_duration;}
+    double getDoubleStanceDuration() const {return _double_stance_duration;}
+    double getMaxInclination() const {return _max_inclination;}
+
+    double getMpcQ() const {return _mpc_Q;}
+    double getMpcR() const {return _mpc_R;}
+    double getHorizonDuration() const {return _horizon_duration;}
+    double getZmpOffset() const {return _zmp_offset;}
+
+private:
+
+    bool parseYAML(YAML::Node yaml_node);
 
     /* init parameters */
-    double initial_lowering;
-    bool first_side_step;
-    double lean_forward;
+    double _initial_lowering;
+    bool _first_side_step;
+    double _lean_forward;
 
     /* walking parameters */
-    double clearance_height;
-    double step_duration;
-    double double_stance_duration;
-    double max_inclination;
+    double _step_clearance;
+    double _step_duration;
+    double _double_stance_duration;
+    double _max_inclination;
 
     /* mpc parameters */
-    double mpc_Q;
-    double mpc_R;
-    double horizon_duration;
-    double zmp_offset;
+    double _mpc_Q;
+    double _mpc_R;
+    double _horizon_duration;
+    double _zmp_offset;
 
 };
 
--- src/param/param.cpp
+++ src/param/param.cpp
@@ -3,64 +3,59 @@
 Walker::Param::Param()
 {
     /* init parameters */
-    initial_lowering = -0.12;
-    first_side_step = 0;
-    lean_forward = 0;
+    _initial_lowering = -0.12;
+    _first_side_step = 0;
+    _lean_forward = 0;
 
     /* walking parameters */
-    clearance_height = 0.1;
-    step_duration = 2;
-    double_stance_duration = 0;
-    max_inclination = 0.1;
+    _step_clearance = 0.1;
+    _step_duration = 2;
+    _double_stance_duration = 0;
+    _max_inclination = 0.1;
 
     /* mpc parameters */
-    mpc_Q = 1000000;
-    mpc_R = 1;
-    horizon_duration = 5;
-    zmp_offset = 0;
+    _mpc_Q = 1000000;
+    _mpc_R = 1;
+    _horizon_duration = 5;
+    _zmp_offset = 0;
 
 }
 
 
-Walker::Param::Param(ros::NodeHandle nh)
+Walker::Param::Param(ros::NodeHandle nh) : Param()
 {
-    /* initialize default */
-    Param();
 
-    std::string first_side_step_string;
-    std::string default_first_side_step_string = "Left";
-    /* init parameters */
-    initial_lowering = nh.param("initial_lowering", initial_lowering);
-    first_side_step_string = nh.param("first_step_side", default_first_side_step_string);
-    lean_forward = nh.param("lean_forward", lean_forward);
+    std::string pd_name = "/walking_parameters";
+    std::string walking_parameters;
 
-    /* walking parameters */
-    clearance_height = nh.param("clearance_step", clearance_height);
-    step_duration = nh.param("duration_step", step_duration);
-    double_stance_duration = nh.param("double_stance_duration", double_stance_duration);
-    max_inclination = nh.param("max_inclination", max_inclination);
-
-    /* mpc parameters */
-    mpc_Q = nh.param("mpc_Q", mpc_Q);
-    mpc_R = nh.param("mpc_R", mpc_R);
-    horizon_duration = nh.param("duration_horizon", horizon_duration);
-    zmp_offset = nh.param("zmp_offset", zmp_offset);
-
-    if (first_side_step_string == "Left")
-        first_side_step = 0;
-    else if (first_side_step_string == "Right")
-        first_side_step = 1;
-    else std::cout << "unknown side starting command" << std::endl;
+    if(!nh.hasParam(pd_name) ||
+            !nh.getParam(pd_name,
+                         walking_parameters))
+    {
+        throw std::runtime_error("problem_description '" + pd_name + "' parameter missing");
+    }
+    else
+    {
+        YAML::Node yaml_node = YAML::Load(walking_parameters);
+        parseYAML(yaml_node);
+    }
 }
 
-Walker::Param::Param(YAML::Node yaml_node)
+Walker::Param::Param(YAML::Node yaml_node) : Param()
 {
-    /* initialize default */
-    Param();
+    parseYAML(yaml_node);
+}
 
+bool Walker::Param::parseYAML(YAML::Node yaml_node)
+{
     std::string init_par = "init_parameters";
+//    std::vector<std::string> init_par_vec = {"initial_lowering", "first_step_side", "lean_forward"};
+
     std::string walk_par = "walking_parameters";
-    std::string mpc_par = "mpc_parameters";
+//     std::vector<std::string> walk_par_vec = {"step_clearance", "step_duration", "double_stance_duration", "max_inclination"};
+
+     std::string mpc_par = "mpc_parameters";
+//     std::vector<std::string> mpc_par_vec = {"Q", "R", "horizon_duration", "zmp_offset"};
 
     YAML::Node init_parameters = yaml_node[init_par];
     if(!init_parameters)
@@ -71,16 +66,36 @@ Walker::Param::Param(YAML::Node yaml_node)
     {
         if(!init_parameters["initial_lowering"])
         {
-            std::cout << "Missing in '"<< init_par << "': 'initial_lowering'. Using default --> " << initial_lowering << std::endl;
+            std::cout << "Missing in '"<< init_par << "': 'initial_lowering'. Using default --> " << _initial_lowering << std::endl;
         }
+        else
+        {
+            _initial_lowering = init_parameters["initial_lowering"].as<double>();
+        }
+
         if(!init_parameters["first_step_side"])
         {
-            std::cout << "Missing in '"<< init_par << "': 'first_step_side'. Using default --> " << first_side_step << std::endl;
+            std::cout << "Missing in '"<< init_par << "': 'first_step_side'. Using default --> " << _first_side_step << std::endl;
         }
+        else
+        {
+            std::string temp = init_parameters["first_step_side"].as<std::string>();
+            if (temp == "Left")
+                _first_side_step = 0;
+            else if (temp == "Right")
+                _first_side_step = 1;
+            else std::cout << "unknown side starting command" << std::endl;
+        }
+
         if(!init_parameters["lean_forward"])
         {
-            std::cout << "Missing in '"<< init_par << "': 'lean_forward'. Using default --> " << lean_forward << std::endl;
+            std::cout << "Missing in '"<< init_par << "': 'lean_forward'. Using default --> " << _lean_forward << std::endl;
         }
+        else
+        {
+            _lean_forward = init_parameters["lean_forward"].as<double>();
+        }
+
     }
 
     YAML::Node walking_parameters = yaml_node["walking_parameters"];
@@ -92,19 +107,38 @@ Walker::Param::Param(YAML::Node yaml_node)
     {
         if(!walking_parameters["step_clearance"])
         {
-            std::cout << "Missing in '" << walk_par << "': 'step_clearance'. Using default --> " << clearance_height << std::endl;
+            std::cout << "Missing in '" << walk_par << "': 'step_clearance'. Using default --> " << _step_clearance << std::endl;
         }
+        else
+        {
+            _step_clearance = walking_parameters["step_clearance"].as<double>();
+        }
+
         if(!walking_parameters["step_duration"])
         {
-            std::cout << "Missing in '" << walk_par << "': 'first_step_side'. Using default --> " << step_duration << std::endl;
+            std::cout << "Missing in '" << walk_par << "': 'step_duration'. Using default --> " << _step_duration << std::endl;
         }
+        else
+        {
+            _step_duration = walking_parameters["step_duration"].as<double>();
+        }
+
         if(!walking_parameters["double_stance_duration"])
         {
-            std::cout << "Missing in '" << walk_par << "': 'double_stance_duration'. Using default --> " << double_stance_duration << std::endl;
+            std::cout << "Missing in '" << walk_par << "': 'double_stance_duration'. Using default --> " << _double_stance_duration << std::endl;
         }
+        else
+        {
+            _double_stance_duration = walking_parameters["double_stance_duration"].as<double>();
+        }
+
         if(!walking_parameters["max_inclination"])
         {
-            std::cout << "Missing in '" << walk_par << "': 'max_inclination'. Using default --> " << max_inclination << std::endl;
+            std::cout << "Missing in '" << walk_par << "': 'max_inclination'. Using default --> " << _max_inclination << std::endl;
+        }
+        else
+        {
+            _max_inclination = walking_parameters["max_inclination"].as<double>();
         }
     }
 
@@ -117,20 +151,40 @@ Walker::Param::Param(YAML::Node yaml_node)
     {
         if(!mpc_parameters["Q"])
         {
-            std::cout << "Missing in '" << mpc_par << "': 'Q'. Using default --> " << mpc_Q << std::endl;
+            std::cout << "Missing in '" << mpc_par << "': 'Q'. Using default --> " << _mpc_Q << std::endl;
         }
+        else
+        {
+            _mpc_Q = mpc_parameters["Q"].as<double>();
+        }
+
         if(!mpc_parameters["R"])
         {
-            std::cout << "Missing in '" << mpc_par << "':'R'. Using default --> " << mpc_R << std::endl;
+            std::cout << "Missing in '" << mpc_par << "':'R'. Using default --> " << _mpc_R << std::endl;
         }
+        else
+        {
+            _mpc_R = mpc_parameters["R"].as<double>();
+        }
+
         if(!mpc_parameters["horizon_duration"])
         {
-            std::cout << "Missing in '" << mpc_par << "':'horizon_duration'. Using default --> " << horizon_duration << std::endl;
+            std::cout << "Missing in '" << mpc_par << "':'horizon_duration'. Using default --> " << _horizon_duration << std::endl;
         }
+        else
+        {
+            _horizon_duration = mpc_parameters["horizon_duration"].as<double>();
+        }
+
         if(!mpc_parameters["zmp_offset"])
         {
-            std::cout << "Missing in '" << mpc_par << "':'zmp_offset'. Using default --> " << zmp_offset << std::endl;
+            std::cout << "Missing in '" << mpc_par << "':'zmp_offset'. Using default --> " << _zmp_offset << std::endl;
+        }
+        else
+        {
+            _zmp_offset = mpc_parameters["zmp_offset"].as<double>();
         }
     }
 
+    return true;
 }
