84e16699b0660a4c3f1eb49b3f904e10ff5eb107
francesco.ruscelli <francesco.ruscelli@iit.it>
2020-01-28 18:56:09 +0100

    added q_fake and distance_ankle_com to step_state

--- include/robot/step_state.h
+++ include/robot/step_state.h
@@ -9,17 +9,19 @@ struct StepState {
 
     StepState() :
         q(0),
+        q_fake(0),
         q_min(0),
         q_max(0),
         step_duration(0),
         step_clearance(0),
         zmp_val_current(0),
         zmp_val_next(0),
-        height_com(0)
+        height_com(0),
+        distance_ankle_com(0)
     {
     }
 
-    double q, q_min, q_max;
+    double q, q_fake, q_min, q_max;
 
     double step_duration, step_clearance;
 
@@ -27,14 +29,19 @@ struct StepState {
     double zmp_val_next;
 
     double height_com;
+    double distance_ankle_com;
 
     void log(XBot::MatLogger::Ptr logger)
     {
         logger->add("q", q);
+        logger->add("q_fake", q_fake);
         logger->add("q_min", q_min);
         logger->add("q_max", q_max);
         logger->add("zmp_current", zmp_val_current);
         logger->add("zmp_next", zmp_val_next);
+        logger->add("height_com", height_com);
+        logger->add("distance_ankle_com", distance_ankle_com);
+
     }
 
 };
