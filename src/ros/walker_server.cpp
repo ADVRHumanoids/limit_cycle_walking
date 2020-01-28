80f728e33b5d5743e00865b74c4939b25b2b672c
francesco.ruscelli <francesco.ruscelli@iit.it>
2020-01-28 19:00:52 +0100

    added sleep in server, to get the right loop rate

--- src/ros/walker_server.cpp
+++ src/ros/walker_server.cpp
@@ -6,11 +6,16 @@ using namespace XBot::Cartesian;
 int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "walker_server_node");
+    ros::NodeHandle nh("~");
+
+    ros::Rate loop_rate(100);
 
     WalkerExecutor exec;
 
     while(ros::ok())
     {
         exec.run();
+        loop_rate.sleep();
+
     }
 }
