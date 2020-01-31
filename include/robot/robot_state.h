fee5ffb6d9c5e4a06e3fbf87b42bf1778cc406fb
francesco.ruscelli <francesco.ruscelli@iit.it>
2020-01-31 14:42:48 +0100

    changed log names

--- include/robot/robot_state.h
+++ include/robot/robot_state.h
@@ -38,11 +38,11 @@ struct RobotState
     void log(std::string name, XBot::MatLogger::Ptr logger)
     {
         logger->add(name + "_com_vel", com_vel);
-        logger->add(name + "_lfoot", world_T_foot[0].translation());
-        logger->add(name + "_rfoot", world_T_foot[1].translation());
+        logger->add(name + "_l_foot", world_T_foot[0].translation());
+        logger->add(name + "_r_foot", world_T_foot[1].translation());
         logger->add(name + "_waist", world_T_waist.translation());
-        logger->add(name + "_lankle_to_com", ankle_T_com[0].translation());
-        logger->add(name + "_rankle_to_com", ankle_T_com[1].translation());
+        logger->add(name + "_l_ankle_to_com", ankle_T_com[0].translation());
+        logger->add(name + "_r_ankle_to_com", ankle_T_com[1].translation());
         logger->add(name + "_com_pos", world_T_com);
     }
 };
